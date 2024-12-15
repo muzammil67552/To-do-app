1bd0a6a9420ce7fd9489375960c097b7815d2186
Muhammad Muzammil <yoursmuzammil16070@gmail.com>
2024-12-15 22:30:29 +0500

    first commit

--- client/src/utilis/api.jsx
+++ client/src/utilis/api.jsx
@@ -1,1 +1,1 @@
-export const BaseURL = 'http://localhost:4000/api'
+export const BaseURL = 'https://backend-murex-five.vercel.app/api'
--- server/server.js
+++ server/server.js
@@ -1,38 +1,37 @@
-const express = require ("express");
-const cors = require ("cors");
-const { default: mongoose } = require("mongoose");
-require("dotenv").config()
-const router = require ("./route/routes")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const router = require("./route/routes");
 
+const PORT = process.env.PORT || 5000;
+const app = express();
 
+// Middleware
+app.use(cors({
+  origin: 'https://mernstacktodoapp12.netlify.app/',
+  methods: ['GET', 'POST'],
+}));
+app.use(express.json());
 
+// Mongoose Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("Error in connecting:", err);
+  });
 
-const PORT = process.env.PORT || 5000
-const app = express()
+// Route breakpoint API
+app.use('/api', router);
 
-//middleware
-app.use(cors())
-app.use(express.json())
-
-
-//mongoose Connection
-mongoose
-.connect(process.env.MONGO_URI)
-.then(() =>{
-    console.log("connected Successfully")
-})
-.catch((err) =>{
-    console.log("error in connecting")
+// Display response on localhost
+app.get('/', (req, res) => {
+  res.send("Hello World");
 });
 
-// route breakpoint api
-app.use('/api', router)
-
-//display on localhost screen response
-app.get('/', (req, res) =>{
-    res.send("Hello World")
-})
-app.listen (PORT, (req, res) =>{
-    console.log(`Server is Listen ${PORT}`);
-    
-})
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
